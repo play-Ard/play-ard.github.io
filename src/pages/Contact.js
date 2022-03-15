fa2f40af58bedaf16c9756d7d7f5663a722ad727
Erhan <80072131+erhan-namli@users.noreply.github.com>
2022-03-16 02:39:06 +0300

    update

---
+++ src/pages/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from "../components/Footer";
+
+const Contact = () => {
+  return (
+    <div>
+
+        <Header></Header>
+
+        <Footer></Footer>
+
+
+
+    </div>
+  )
+}
+
+export default Contact
