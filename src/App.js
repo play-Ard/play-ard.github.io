37229126364124cca35cca97cb03b82261c6e8cb
Erhan <80072131+erhan-namli@users.noreply.github.com>
2022-08-01 04:00:18 +0300

    Remove admin panel

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { ProjectsContext } from './Context/ProjectsContext';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
+
 import {BrowserRouter,  Routes, Route, HashRouter} from 'react-router-dom'
 import HomePage from './Pages/HomePage';
 
@@ -14,14 +15,11 @@ import MakeaGame from './Pages/MakeaGame'
 import Contact from './Pages/Contact'
 import Shop from './Pages/Shop'
 
-import Falan from './Pages/Falan'
 
 import LoginPage from './Pages/LoginPage'
 
 
-import TempLayout from './Pages/TempLayout'
 
-import AdminPanelLayout from './Layouts/AdminPanelLayout';
 
 // const AdminPanelLayout = React.lazy(() => import('./Layouts/AdminPanelLayout'))
 
@@ -46,20 +44,18 @@ function App() {
           <Suspense fallback={loading}>  {/* if the page loads, this action happens */}
 
             <Routes>
-              <Route exact path='/' element={<HomePage/>}/>
-              <Route exact path= { '/about' } element={<About/>}/>s
-              <Route exact path= { '/login' } element={<LoginPage/>}/>
-              <Route exact path= { '/millionaire' } element={<Millionaire/>}/>
-              <Route exact path= { '/layoutdesigner' } element={<LayoutDesigner/>}/>
-              <Route exact path= { '/documentation' } element={<Documentation/>}/>
-              <Route exact path= { '/makeagame' } element={<MakeaGame/>}/>
-              <Route exact path= { '/contact' } element={<Contact/>}/>
-              <Route exact path= { '/shop' } element={<Shop/>}/>
-              <Route exact path= { '/games' } element={<Games/>}/>
+              <Route path='/' element={<HomePage/>}/>
+              <Route path= { '/about' } element={<About/>}/>s
+              <Route path= { '/login' } element={<LoginPage/>}/>
+              <Route path= { '/millionaire' } element={<Millionaire/>}/>
+              <Route path= { '/layoutdesigner' } element={<LayoutDesigner/>}/>
+              <Route path= { '/documentation' } element={<Documentation/>}/>
+              <Route path= { '/makeagame' } element={<MakeaGame/>}/>
+              <Route path= { '/contact' } element={<Contact/>}/>
+              <Route path= { '/shop' } element={<Shop/>}/>
+              <Route path= { '/games' } element={<Games/>}/>
 
-              <Route exact path= { '/adminpanel' } element={<AdminPanelLayout/>}/>
-
-              <Route exact path={ '/templayout' } element={<TempLayout/>}/>
+  
 
             </Routes>
 
